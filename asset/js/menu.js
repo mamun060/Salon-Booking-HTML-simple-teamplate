f5ef5b2a05d89850381f07ed750a57ebe343cb44
mamun060 <obujraju420@gmail.com>
2022-01-08 12:19:20 +0600

    simple salon ui template for client

---
+++ asset/js/menu.js
@@ -0,0 +1,25 @@
+const navbarMenu = document.getElementById("menu");
+const burgerMenu = document.getElementById("burger");
+const bgOverlay = document.getElementById("overlay");
+
+// Show Menu when Click the Burger
+// Hide Menu when Click the Overlay
+if (burgerMenu && navbarMenu && bgOverlay) {
+	burgerMenu.addEventListener("click", () => {
+		navbarMenu.classList.toggle("is-active");
+		bgOverlay.classList.toggle("is-active");
+	});
+
+	bgOverlay.addEventListener("click", () => {
+		navbarMenu.classList.toggle("is-active");
+		bgOverlay.classList.toggle("is-active");
+	});
+}
+
+// Hide Menu when Click the Links
+document.querySelectorAll(".menu-link").forEach((link) => {
+	link.addEventListener("click", () => {
+		navbarMenu.classList.remove("is-active");
+		bgOverlay.classList.remove("is-active");
+	});
+});
